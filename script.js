0ffbc9e96eb7fbb67b8204aa5c790bd2acfd52ce
Kelyton Gonçalves Silva <79583704+kelyton-sillva@users.noreply.github.com>
2022-04-03 15:26:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 function carregar (){
 var msg = window.document.getElementById("msg")
 var img = window.document.getElementById('imagem')
+var bom = window.document.getElementById('bom')
 var data = new Date()
 var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora} horas`
+var minutos = data.getMinutes()
+msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos`
 
-if (hora >= 0 && hora < 12){
+
+if (hora >= 5 && hora < 12){
 img.src='manha.jpg'               // BOM DIA  
 document.body.style.background = '#FFFF00'
+bom.innerHTML = 'Bom dia'
+
 }
-else if (hora >= 12 && hora <=18){
+else if (hora >= 12 && hora <18){
     img.src='tarde.jpg'           // BOA TARDE
     document.body.style.background = '#FF5A00'
+    bom.innerHTML = 'Boa tarde'
 }
 else {
     img.src='noite.jpg'       //BOA NOITE
     document.body.style.background = '#120A8F'
+    bom.innerHTML = 'Boa noite'
 }
+
+
 
 }
